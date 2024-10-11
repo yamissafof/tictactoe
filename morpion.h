415e6fdf7a4a1a7133340bc7a54031f2b0ae0f11
yamissafof <146427226+yamissafof@users.noreply.github.com>
2024-10-11 15:52:56 +0200

    Create morpion.h

---
+++ morpion.h
@@ -0,0 +1,9 @@
+#ifndef MORPION_H
+#define MORPION_H
+
+void printBoard(char board[9]);
+int checkWin(char board[9]);
+// int verifierMatchNul(char board[9]);
+void initializeBoard(char board[9]);
+
+#endif
